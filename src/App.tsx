78bb990518f402dd3edada8137728e1e8660246a
EndlessC0ding <endlesscoding@yahoo.com>
2025-12-25 23:36:22 +0300

    cr a/stls/thms a/A/(chThm)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Todolist } from "@/widgets/Todolist";
+import { useState } from "react";
+import { AppButton } from "./shared/ui/AppButton/AppButton";
+type Theme = "light" | "dark";
 
 export const App = () => {
+  const [theme, setTheme] = useState<Theme>("dark");
   return (
-    <div className="app">
+    <div className={`app app_${theme}_theme`}>
+      <AppButton onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {"Theme"}
+      </AppButton>
       <Todolist />
     </div>
   );
